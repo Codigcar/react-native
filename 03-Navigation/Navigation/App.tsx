4938998c97eee4886ed077b5a8407bf670df22ea
Carlos Castilla <34387852+Codigcar@users.noreply.github.com>
2021-06-20 11:12:36 -0500

    03 - Added drawer navigation inicio
    menu que se abre de izquierda a derecha

--- 03-Navigation/Navigation/App.tsx
+++ 03-Navigation/Navigation/App.tsx
@@ -3,11 +3,14 @@ import React from 'react'
 import { View, Text, SafeAreaView } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { StackNavigator } from './src/navigator/StackNavigator';
+import { MenuLateralBasico } from './src/navigator/MenuLateralBasico';
 
 const App = () => {
     return (
       <NavigationContainer>
-        <StackNavigator />
+        {/* <StackNavigator /> */}
+        <MenuLateralBasico />
+        
       </NavigationContainer>
     )
 }

46660cedd49c94b0f8092fa4aca2d0106102210c
Carlos Castilla <34387852+Codigcar@users.noreply.github.com>
2021-06-19 21:44:49 -0500

    03 - Dando estilos y manejando el titulo y textos

--- 03-Navigation/Navigation/src/Screen/Pagina1Screen.tsx
+++ 03-Navigation/Navigation/src/Screen/Pagina1Screen.tsx
@@ -8,7 +8,7 @@ interface Props extends StackScreenProps<any, any>{};
  export const Pagina1Screen = ({navigation}: Props) => {
      return (
          <View style={styles.globalMargin}>
-             <Text>Pagina1</Text>
+             <Text style={styles.title}>Pagina1</Text>
              <Button title="Ir a pagina2" onPress={()=> navigation.navigate('Pagina2Screen')} color='red' />
          </View>
      )
--- 03-Navigation/Navigation/src/Screen/Pagina2Screen.tsx
+++ 03-Navigation/Navigation/src/Screen/Pagina2Screen.tsx
@@ -1,11 +1,19 @@
 import { useNavigation } from '@react-navigation/native';
 import React from 'react'
+import { useEffect } from 'react';
 import { View, Text, Button } from 'react-native';
 import { styles } from '../theme/Styles';
 
  export const Pagina2Screen = () => {
     //  Reemplaza a los "props" pero sobrecarga la aplicación usando este hook
      const navigator = useNavigation();
+
+     useEffect(() => {
+         navigator.setOptions({
+            title:'Tituloo',
+            headerBackTitle:'Atras'
+         })
+     }, [])
 
      return (
          <View style={styles.globalMargin}>

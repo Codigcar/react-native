0db0ccf7b57460e815e9c6ca6088d70158c6834c
Carlos Castilla <34387852+Codigcar@users.noreply.github.com>
2021-06-26 16:16:18 -0500

    04 - Added Icons para Android

--- 04-NavigationApp/Navigation/src/Screen/Tab2Screen.tsx
+++ 04-NavigationApp/Navigation/src/Screen/Tab2Screen.tsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import Icon from 'react-native-vector-icons/Ionicons';
+
 import { View, Text } from 'react-native';
 
 export const Tab2Screen = () => {
@@ -7,6 +9,7 @@ export const Tab2Screen = () => {
     return (
         <View>
             <Text>Tab2Screen</Text>
+            <Icon name="airplane-outline" size={30} color="#900"></Icon>
         </View>
     )
 }

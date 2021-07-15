910a4bfbe244bbbabb3740a8cbd7a11da6e28d2a
Carlos Castilla <34387852+Codigcar@users.noreply.github.com>
2021-07-15 00:17:04 -0500

    05 - Consumir el Context - AuthState

--- 05-Context-Global-React/Navigation/src/Screen/Tab3Screen.tsx
+++ 05-Context-Global-React/Navigation/src/Screen/Tab3Screen.tsx
@@ -1,11 +1,18 @@
 import React from 'react'
+import { useContext } from 'react';
 import { View, Text } from 'react-native';
+import { AuthContext } from '../context/AuthContext';
 
 export const Tab3Screen = () => {
     console.log('Tab3Screen');
+
+    const context = useContext(AuthContext);
+    
     return (
         <View>
             <Text>Tab3Screen</Text>
+            <Text>Dato traido del Context: </Text>
+            <Text>{JSON.stringify(context.authState,null,)}</Text>
         </View>
     )
 }

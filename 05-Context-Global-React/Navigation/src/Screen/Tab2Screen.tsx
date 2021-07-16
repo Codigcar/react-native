27c280025688e96ca6cac8592ede8778bfd04439
Carlos Castilla <34387852+Codigcar@users.noreply.github.com>
2021-07-15 23:51:52 -0500

    05 - Disparar el SignIn en otra pantalla que está dentro del AuthContext

--- 05-Context-Global-React/Navigation/src/Screen/Tab2Screen.tsx
+++ 05-Context-Global-React/Navigation/src/Screen/Tab2Screen.tsx
@@ -1,16 +1,26 @@
 import React from 'react'
 import Icon from 'react-native-vector-icons/Ionicons';
 
-import { View, Text } from 'react-native';
-import { colores } from '../theme/Styles';
+import { View, Text, Button } from 'react-native';
+import { colores, styles } from '../theme/Styles';
+import { useContext } from 'react';
+import { AuthContext } from '../context/AuthContext';
 
 export const Tab2Screen = () => {
     console.log('Tab2Screen');
+
+    const {signIn, authState} = useContext(AuthContext);
     
     return (
         <View>
             <Text>Tab2Screen</Text>
             <Icon name="airplane-outline" size={80} color={colores.primary}></Icon>
+            <View style={styles.globalMargin}>
+                <Text>Login</Text>
+                {
+                   !authState.isLoggedIn && <Button title="SignIng" onPress={signIn}></Button>
+                }
+            </View>
         </View>
     )
 }

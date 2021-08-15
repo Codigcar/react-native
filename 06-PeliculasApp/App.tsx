bde0c40ceb402f8752f8ff1c348bcbf827415189
Carlos Castilla <34387852+Codigcar@users.noreply.github.com>
2021-08-15 04:51:43 -0500

    06 - Config SafeArea

--- 06-PeliculasApp/App.tsx
+++ 06-PeliculasApp/App.tsx
@@ -2,13 +2,16 @@ import { NavigationContainer } from '@react-navigation/native';
 import React from 'react'
 
 import 'react-native-gesture-handler';
+import { SafeAreaView } from 'react-native-safe-area-context';
 import { MyStackNavigation } from './src/navigation/Navigation';
 
 const App = () => {
   return (
-    <NavigationContainer>
-      <MyStackNavigation />
-    </NavigationContainer>
+    <SafeAreaView style={{flex:1, backgroundColor:'red'}}>
+      <NavigationContainer>
+        <MyStackNavigation />
+      </NavigationContainer>
+    </SafeAreaView>
   )
 }
 

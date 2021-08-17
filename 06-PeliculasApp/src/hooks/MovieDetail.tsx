b92134b2cf2327dc072dad4e440ef0bb2f27658e
Carlos Castilla <34387852+Codigcar@users.noreply.github.com>
2021-08-17 00:06:52 -0700

    06 - Added info de la Historia y Presupuesto en millones

--- 06-PeliculasApp/src/hooks/MovieDetail.tsx
+++ 06-PeliculasApp/src/hooks/MovieDetail.tsx
@@ -9,17 +9,39 @@ interface Props {
     creditos: CredistInterface[]
 }
 
-export const MovieDetail = ({movie, creditos}:Props) => {
+export const MovieDetail = ({ movie, creditos }: Props) => {
     return (
         <>
             {/* Detalles */}
-            <View style={{marginHorizontal:20, flexDirection:'row'}}>
-                <Icon name="star-outline" color="grey" size={16} />
-                <Text>{movie.vote_average}</Text>
-                    <Text style={{marginLeft:5}}>
+            <View style={{ marginHorizontal: 20 }}>
+                <View style={{ flexDirection: 'row' }}>
+
+                    <Icon name="star-outline" color="grey" size={16} />
+                    <Text style={{ marginLeft: 10 }}>
+                        {movie.vote_average}
+                    </Text>
+                    <Text style={{ marginLeft: 5 }}>
                         - {movie.genres.map(genero => genero.name).join(', ')}
                     </Text>
+                </View>
+
+                {/* Historia */}
+                <Text style={{ fontSize: 23, marginTop: 10, fontWeight: 'bold' }}>
+                    Historia
+                </Text>
+                <Text style={{ fontSize: 16 }}>
+                    {movie.overview}
+                </Text>
+                <Text style={{ fontSize: 23, marginTop: 10, fontWeight: 'bold' }}>
+                    Presupuesto
+                </Text>
+                <Text>
+                    {movie.budget}
+                </Text>
+
             </View>
-        </>            
+
+
+        </>
     )
 }
